a6b2161f7afb9c8ec7fd55d69d76c88560183085
Gabriel Rodríguez <gabrc52@gmail.com>
2023-04-22 23:59:18 -0400

    Make bash script report on progress

--- export/download_everything.sh
+++ export/download_everything.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo Downloading team list
 python export_teams.py
+echo Downloading user list for desired team
 python export_users.py
+echo Downloading channel list for desired team
 python export_channel_list.py
+echo Downloading all public messages for desired team
 python export_all_channels.py
+echo Downloading all media in those channels
 python download_media.py
