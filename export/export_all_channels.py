1c3f54b2e95fd43b0027905fcdd75a31a986e362
Gabriel Rodríguez <gabrc52@gmail.com>
2023-04-23 00:28:30 -0400

    Block channels with "High Volume" on their name

--- export/export_all_channels.py
+++ export/export_all_channels.py
@@ -15,6 +15,11 @@ channels = json.load(open('channels.json', 'r'))
 for channel in channels:
     print('Downloading channel', channel['display_name'])
 
+    # Hardcoded behavior for my use-case. TODO: generalize to a list of blocked channels or keywords
+    if 'High Volume' in channel['display_name']:
+        print('   skipping')
+        break
+
     # Join if necessary
     mm.add_user_to_channel(channel['id'], own_id)
 
