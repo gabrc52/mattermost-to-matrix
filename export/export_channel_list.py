06f738479355b7693f869784eb315e074d98be56
Gabriel Rodríguez <gabrc52@gmail.com>
2023-04-24 01:18:30 -0400

    Get team ID from API and not config file

--- export/export_channel_list.py
+++ export/export_channel_list.py
@@ -1,7 +1,18 @@
 from login import mm
 from constants import *
 import json
+import os
 
-all_channels = [channel for channel in mm.get_team_channels(TEAM_ID)]
+if not os.path.exists('teams.json'):
+    import export_teams
+    # No need to do anything else, we have created the file now
+
+teams = json.load(open('teams.json', 'r'))
+team_ids = [team['id'] for team in teams]
+
+all_channels = []
+
+for team_id in team_ids:
+    all_channels.extend([channel for channel in mm.get_team_channels(team_id)])
 
 json.dump(all_channels, open('channels.json', 'w'))
