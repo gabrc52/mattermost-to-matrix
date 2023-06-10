d76fa1e5e8b94ea42a4f0da0e6cad72979cc7df1
Gabriel Rodríguez <gabrc52@gmail.com>
2023-06-10 17:19:55 -0400

    Print teams

--- export/export_teams.py
+++ export/export_teams.py
@@ -3,4 +3,5 @@ import json
 
 all_teams = [team for team in mm.get_teams()]
 
+print(f'Found teams {", ".join(team["name"] for team in all_teams)}')
 json.dump(all_teams, open('../downloaded/teams.json', 'w'))
