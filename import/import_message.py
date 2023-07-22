a93a03dde008cab5b463add54d4b0c4476e83410
Gabriel Rodríguez <gabrc52@gmail.com>
2023-07-22 18:43:41 -0400

    fix media in threads

--- import/import_message.py
+++ import/import_message.py
@@ -5,7 +5,8 @@ import markdown
 import mautrix.errors
 from import_user import import_user
 from matrix import config, get_app_service
-from mautrix.types import (BaseFileInfo, EventType, Format, ImageInfo,
+from mautrix.types import (BaseFileInfo, BaseMessageEventContent, EventType,
+                           Format, ImageInfo, MediaMessageEventContent,
                            Membership, MemberStateEventContent, MessageType,
                            RoomNameStateEventContent,
                            RoomTopicStateEventContent, TextMessageEventContent)
@@ -52,48 +53,6 @@ def get_reactions(reactions):
     })
 
 
-async def import_files_in_message(message, room_id, user_api):
-    """
-    Send the files in the Mattermost message as separate Matrix messages.
-
-    Returns the Matrix event ID of the last file sent
-    """
-    for file in message['metadata']['files']:
-        # Upload first
-        filename = f'../downloaded/media/{file["id"]}'
-        with open(filename, 'rb') as f:
-            contents = f.read()
-            image_uri = await user_api.upload_media(contents, file['mime_type'], file['name'])
-
-        if file['mime_type'].startswith('image'):
-            # Images
-            event_id = await user_api.send_image(
-                room_id,
-                url=image_uri,
-                info=ImageInfo(
-                    mimetype=file['mime_type'],
-                    size=file['size'],
-                    height=file['height'],
-                    width=file['width'],
-                ),
-                file_name=file['name'],
-                timestamp=message['create_at'],
-            )
-        else:
-            # Other attachments
-            event_id = await user_api.send_file(
-                room_id,
-                url=image_uri,
-                info=BaseFileInfo(
-                    mimetype=file['mime_type'],
-                    size=file['size'],
-                ),
-                file_name=file['name'],
-                timestamp=message['create_at'],
-            )
-    return event_id
-
-
 async def import_message(message, room_id, topic_equivalent, state: MessageState, thread_sizes = None):
     """
     Import a specific message from the Mattermost JSON format
@@ -116,6 +75,7 @@ async def import_message(message, room_id, topic_equivalent, state: MessageState
     app_service = get_app_service()
     api = app_service.bot_intent()
 
+    # TODO: honor from_webhook, from_bot, override_username, webhook_display_name(?)
     user_mxid = await import_user(message['user_id'])
     user_api = app_service.intent(user_mxid)
 
@@ -128,6 +88,14 @@ async def import_message(message, room_id, topic_equivalent, state: MessageState
             matrix_reply_to = state.matrix_event_id[mattermost_reply_to]
             state.most_recent_message_in_thread[message['root_id']] = message['id']
         
+        def set_reply_or_thread(content: BaseMessageEventContent):
+            if message['root_id']:                
+                if thread_equivalent == 'reply':
+                    content.set_reply(matrix_reply_to)
+                else:
+                    content.set_thread_parent(thread_parent=matrix_thread_root, last_event_in_thread=matrix_reply_to)
+
+        # Handle (rich) text messages
         if message['message']:
             content = TextMessageEventContent(
                 msgtype=MessageType.TEXT,
@@ -136,11 +104,7 @@ async def import_message(message, room_id, topic_equivalent, state: MessageState
                 format=Format.HTML,
             )
             # set reply if needed
-            if message['root_id']:                
-                if thread_equivalent == 'reply':
-                    content.set_reply(matrix_reply_to)
-                else:
-                    content.set_thread_parent(thread_parent=matrix_thread_root, last_event_in_thread=matrix_reply_to)
+            set_reply_or_thread(content)
             # send message
             event_id = await user_api.send_message(
                 room_id,
@@ -148,12 +112,34 @@ async def import_message(message, room_id, topic_equivalent, state: MessageState
                 timestamp=message['create_at']
             )
 
-        # Handle media (TODO: use replies/threads appropriately)
+        # Handle media
         if 'files' in message['metadata']:
-            event_id = await import_files_in_message(message, room_id, user_api)
+            for file in message['metadata']['files']:
+                # Upload first
+                filename = f'../downloaded/media/{file["id"]}'
+                with open(filename, 'rb') as f:
+                    contents = f.read()
+                    file_uri = await user_api.upload_media(contents, file['mime_type'], file['name'])
+                
+                is_image = file['mime_type'].startswith('image')
+
+                # Send message
+                content = MediaMessageEventContent(
+                    msgtype=MessageType.IMAGE if is_image else MessageType.FILE,
+                    body=file['name'],
+                    url=file_uri,
+                    info=ImageInfo(
+                        mimetype=file['mime_type'],
+                        size=file['size'],
+                        height=file['height'],
+                        width=file['width'],
+                    ) if is_image else BaseFileInfo(mimetype=file['mime_type'], size=file['size']),
+                )
+                set_reply_or_thread(content)
+                event_id = await user_api.send_message(room_id, content)
 
         # Handle Slack attachments
-        if message['type'] == 'slack_attachment' and (message.get('props') or {}).get('attachments'):
+        if message['type'] == 'slack_attachment' and 'attachments' in message['props']:
             print('Warning: Slack-type messages are not fully supported. Send an issue/PR if you want better support.', file=sys.stderr)
             for attachment in message['props']['attachments']:
                 event_id = await user_api.send_message(
