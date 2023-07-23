e191107a2c6315deb0df49ace39f464b3de215b7
Gabriel Rodríguez <gabrc52@gmail.com>
2023-07-23 14:08:19 -0400

    download team profile pictures too

--- export/download_profile_pictures.py
+++ export/download_profile_pictures.py
@@ -1,4 +1,8 @@
-from media import download_media, download_profile_picture
+"""
+Download all user and team profile pictures
+"""
+
+from media import download_media, download_profile_picture, download_team_picture
 import os
 import json
 
@@ -7,6 +11,7 @@ if not os.path.exists('../downloaded/pfp'):
     os.mkdir('../downloaded/pfp')
 
 users = json.load(open('../downloaded/users.json', 'r'))
+teams = json.load(open('../downloaded/teams.json', 'r'))
 
 # TODO: this redownloads all pictures every time you run the file
 # It might be good to persist which ones have been downloaded and not been changed
@@ -14,3 +19,7 @@ for user in users:
     if 'last_picture_update' in user:
         # Otherwise the API just returns a generic profile picture
         download_profile_picture(user['id'])
+
+for team in teams:
+    if 'last_team_icon_update' in team:
+        download_team_picture(team['id'])
--- export/media.py
+++ export/media.py
@@ -7,7 +7,7 @@ def download_media(media_id):
 
     Assumes the `media` folder already exists
     """
-    response = mm._get(f'/v4/files/{media_id}', raw=True)
+    response = mm.get_file(media_id)
     with open(f'../downloaded/media/{media_id}', 'wb') as f:
         f.write(response.content)
 
@@ -17,8 +17,20 @@ def download_profile_picture(user_id):
     Dumps the desired profile picture by user ID into a file inside the `pfp`
     folder.
 
-    Assumes the `pfp` folder already exists.
+    Assumes the `pfp` folder already exists, and that the user has a profile picture.
     """
     response = mm._get(f'/v4/users/{user_id}/image', raw=True)
     with open(f'../downloaded/pfp/{user_id}', 'wb') as f:
+        f.write(response.content)
+
+
+def download_team_picture(team_id):
+    """
+    Dumps the desired team profile picture by team ID into a file inside the `pfp`
+    folder.
+
+    Assumes the `pfp` folder already exists, and that the team has a profile picture.
+    """
+    response = mm._get(f'/v4/teams/{team_id}/image', raw=True)
+    with open(f'../downloaded/pfp/{team_id}', 'wb') as f:
         f.write(response.content)
