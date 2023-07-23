161ea8ce60d0ddba61411149f5b2864f2f77ddad
Gabriel Rodríguez <gabrc52@gmail.com>
2023-07-23 13:02:37 -0400

    mitosis create_room

--- import/import_channel.py
+++ import/import_channel.py
@@ -39,12 +39,11 @@ def get_alias_localpart(channel):
     return f"{config.matrix.room_prefix}{team['name']}_{channel['name']}"
 
 
-async def create_channel_from_json(channel):
+async def create_room(alias_localpart, name, power_level_override, creator_mxid):
     """
-    Creates a Mattermost channel with the given channel JSON into a Matrix room.
-    Returns the room ID on Matrix
+    Creates a Matrix room with the given properties, and invites and makes admin the
+    specified people in the config
     """
-    alias_localpart = get_alias_localpart(channel)
     app_service = get_app_service()
     api = app_service.bot_intent()
 
@@ -58,39 +57,15 @@ async def create_channel_from_json(channel):
 
     # If it doesn't exist, create it
     if not already_exists:
-        if channel['creator_id']:
-            creator_mxid = await import_user(channel['creator_id'])
+        if creator_mxid:
             user_api = app_service.intent(creator_mxid)
         else:
             user_api = app_service.bot_intent()
         room_id = await user_api.create_room(
             preset=RoomCreatePreset.PUBLIC,
             alias_localpart=alias_localpart,
-            name=channel['display_name'],
-            power_level_override={
-                'events': {
-                    # everyone has permission to rename rooms in mattermost
-                    "m.room.name": 0,
-                    # everyone has permission to pin in mattermost
-                    "m.room.pinned_events": 0,
-                    # everyone has permission to change the topic
-                    "m.room.topic": 0,
-                    # leave the rest as default (if ommitted, they are not merged into this dict,
-                    # giving everyone permission to do everything - i think?)
-                    "m.room.power_levels": 100,
-                    "m.room.history_visibility": 100,
-                    "m.room.canonical_alias": 50,
-                    "m.room.avatar": 50,
-                    "m.room.tombstone": 100,
-                    "m.room.server_acl": 100,
-                    "m.room.encryption": 100
-                },
-                # everyone can invite
-                'invite': 0,
-                'users': {user: 100 for user in config.matrix.users}               # As per config
-                       | {get_user_mxid_by_localpart(config.matrix.username): 100} # Make ourselves admin
-                       | {user_api.mxid: 100},                                     # Make creator admin
-            },
+            name=name,
+            power_level_override=power_level_override,
         )
         # Invite bot user if needed
         await api.ensure_joined(room_id, bot=user_api) # I see the advantage of this
@@ -100,6 +75,48 @@ async def create_channel_from_json(channel):
         await api.invite_user(room_id, user)
 
     return room_id
+
+
+async def create_channel_from_json(channel):
+    """
+    Creates a Mattermost channel with the given channel JSON into a Matrix room.
+    Returns the room ID on Matrix
+    """
+    alias_localpart = get_alias_localpart(channel)
+    creator_mxid = await import_user(channel['creator_id']) if channel['creator_id'] else None
+    power_level_override = {
+        'events': {
+            # everyone has permission to rename rooms in mattermost
+            "m.room.name": 0,
+            # everyone has permission to pin in mattermost
+            "m.room.pinned_events": 0,
+            # everyone has permission to change the topic
+            "m.room.topic": 0,
+            # leave the rest as default (if ommitted, they are not merged into this dict,
+            # giving everyone permission to do everything - i think?)
+            "m.room.power_levels": 100,
+            "m.room.history_visibility": 100,
+            "m.room.canonical_alias": 50,
+            "m.room.avatar": 50,
+            "m.room.tombstone": 100,
+            "m.room.server_acl": 100,
+            "m.room.encryption": 100
+        },
+        # everyone can invite
+        'invite': 0,
+        'users': {user: 100 for user in config.matrix.users}               # As per config
+                | {get_user_mxid_by_localpart(config.matrix.username): 100} # Make ourselves admin
+    }
+    # Make creator admin
+    if creator_mxid:
+        power_level_override['users'] |= {creator_mxid: 100}
+    return await create_room(
+        alias_localpart=alias_localpart,
+        name=channel['display_name'],
+        power_level_override=power_level_override,
+        creator_mxid=creator_mxid,
+    )
+    
 
 
 async def create_channel(channel):
@@ -154,9 +171,6 @@ async def import_channel(channel_id):
             
             bar.next()
 
-    # Close the session when done
-    await get_app_service().session.close()
-
 
 if __name__ == '__main__':
     import sys
@@ -166,3 +180,5 @@ if __name__ == '__main__':
         exit(1)
     channel_id = sys.argv[1]
     asyncio.run(import_channel(channel_id))
+    # Close the session when done
+    asyncio.run(get_app_service().session.close())
